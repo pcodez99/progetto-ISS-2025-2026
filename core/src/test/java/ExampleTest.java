3d64bb83eb615ee8f440a94b4081f101257b010c
fcascino27-debug <fcascino27@gmail.com>
2026-05-03 18:31:38 +0200

    Cartella test ed esempio funzionante

---
+++ core/src/test/java/ExampleTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class ExampleTest {
+    @Test
+    public void testBase(){
+        assertTrue(true, "Il test è stato eseguito correttamente");
+    }
+}
