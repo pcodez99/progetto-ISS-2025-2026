c5ab30d839aa8e0f4c6a40c672661df126596ea7
daniele-r22 <279198689+daniele-r22@users.noreply.github.com>
2026-05-09 12:57:13 +0200

    Aggiunta metodo test per le opzioni del menu (RED)

--- core/src/test/java/io/github/iss_2025_2026/model/MainMenuModelTest.java
+++ core/src/test/java/io/github/iss_2025_2026/model/MainMenuModelTest.java
@@ -8,4 +8,13 @@ public class MainMenuModelTest {
         MainMenuModel model = new MainMenuModel();
         assertNotNull(model,"Il modello dovrebbe essere istanziato");
     }
+    //Test per verificare se il modello espone correttamente le 4 azioni fondamentali richieste
+    @Test
+    public void testMenuActionsPresence(){
+        MainMenuModel model= new MainMenuModel();
+        //Otteniamo la lista delle azioni disponibili
+        MainMenuModel.MenuAction[] actions = model.getAvailableActions();
+        // verifichiamo che ci siano le 4 azioni della US
+        assertEquals(4,actions.length,"Il menu deve avere esattamente 4 opzioni principali");
+    }
 }
