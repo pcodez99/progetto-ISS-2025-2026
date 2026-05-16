3e4e1e694df7b18842d48f4f01fe2648c6b8521a
pcodez <pietrozarboprivato@gmail.com>
2026-05-16 11:44:24 +0200

    impedisce al gioco di partire se mancano le configs

--- core/src/main/java/io/github/iss_2025_2026/Main.java
+++ core/src/main/java/io/github/iss_2025_2026/Main.java
@@ -1,10 +1,10 @@
 package io.github.iss_2025_2026;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
 import io.github.iss_2025_2026.controller.GameController;
-import io.github.iss_2025_2026.controller.MainMenuController;
 import io.github.iss_2025_2026.model.GameModel;
-import io.github.iss_2025_2026.model.MainMenuModel;
 import io.github.iss_2025_2026.view.MainMenuScreen;
 
 /**
@@ -14,6 +14,17 @@ import io.github.iss_2025_2026.view.MainMenuScreen;
 public class Main extends Game {
     @Override
     public void create() {
+
+        String charactersPath = "configs/characters.yaml";
+        String abilitiesPath = "configs/abilities/";
+
+        if (!Gdx.files.internal(charactersPath).exists() || !Gdx.files.internal(abilitiesPath).exists()) {
+            Gdx.app.error("CharacterFactory", "CRITICAL: characters.yaml or abilities.yaml NOT FOUND at "
+                    + charactersPath + " or " + abilitiesPath);
+            // TO DO: AGGIUNGERE WARNING E CHIUDERE IL GIOCO SE MANCANO LE CONFIGS
+            return;
+        }
+
         // Initialize global MVC components
         GameModel gameModel = new GameModel();
         GameController gameController = new GameController(gameModel);
