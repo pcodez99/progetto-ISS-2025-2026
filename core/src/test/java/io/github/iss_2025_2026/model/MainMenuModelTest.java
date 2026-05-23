ef40465c42c60a13fa33fac365b121c89f60f010
daniele-r22 <279198689+daniele-r22@users.noreply.github.com>
2026-05-23 13:06:09 +0200

    Aggiunta unità di test nella classe MainMenuModel(RED)

--- core/src/test/java/io/github/iss_2025_2026/model/MainMenuModelTest.java
+++ core/src/test/java/io/github/iss_2025_2026/model/MainMenuModelTest.java
@@ -19,4 +19,16 @@ public class MainMenuModelTest {
         // Verify that there are the 4 actions
         assertEquals(4, actions.length, "The menu must have exactly 4 main options");
     }
+
+    @Test
+    public void testMenuActionsPresenceWhenRunning() {
+        MainMenuModel model = new MainMenuModel();
+        // Get available actions when game is running (pause menu)
+        MainMenuModel.MenuAction[] actions = model.getAvailableActions(true);
+        // Verify that there are exactly 3 options and in the correct order
+        assertEquals(3, actions.length, "The pause menu must have exactly 3 options");
+        assertEquals(MainMenuModel.MenuAction.CONTINUE_GAME, actions[0]);
+        assertEquals(MainMenuModel.MenuAction.SETTINGS, actions[1]);
+        assertEquals(MainMenuModel.MenuAction.RETURN_TO_MAIN_MENU, actions[2]);
+    }
 }
